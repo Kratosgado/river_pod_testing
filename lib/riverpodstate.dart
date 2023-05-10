fe3cbf8f866562c184c6616f2138ed64c4899a3f
Prince Mbeah Essilfie <pmessilfie@st.knust.edu.gh>
2023-05-10 15:02:15 -0700

    riverpodstate updated, hoping to work

--- lib/riverpodstate.dart
+++ lib/riverpodstate.dart
@@ -1,5 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final counterPrvider = Provider<int>(
-  (ref) => 0,
+  (ref) {
+    return 0;
+  },
 );
+
+final counterProvider = Provider<Counter>((ref) {
+  return Counter(counter: 0);
+});
+
+class Counter {
+  int counter;
+  Counter({required this.counter});
+
+  void increase() {
+    counter++;
+  }
+}
