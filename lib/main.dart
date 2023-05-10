fb58a219130c0ca4d2ca5ca7e4fdc936a3a4e02d
Prince Mbeah Essilfie <pmessilfie@st.knust.edu.gh>
2023-05-10 14:44:21 -0700

    riverpod added to main but does not increase

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:river_pod_testing/riverpodstate.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final count = ref.watch(counterPrvider);
+
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '$count',
+              ),
+              MaterialButton(
+                  elevation: 10,
+                  color: Colors.tealAccent,
+                  child: const Text('increase'),
+                  onPressed: () {
+                    ref.read(counterPrvider);
+                  })
+            ],
+          ),
         ),
       ),
     );
